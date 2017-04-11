91b7293574578f046d453f6860224c82160cad9b
Sergey Zhukov <svg@ngs.ru>
2017-04-11 13:30:07 +0700

    Initial commit

---
+++ RedisGeo/RedisGeo/AppHost.cs
@@ -0,0 +1,30 @@
+﻿using Funq;
+using ServiceStack;
+using RedisGeo.ServiceInterface;
+using ServiceStack.Redis;
+
+namespace RedisGeo
+{
+    //VS.NET Template Info: https://servicestack.net/vs-templates/EmptyAspNet
+    public class AppHost : AppHostBase
+    {
+        /// <summary>
+        /// Base constructor requires a Name and Assembly where web service implementation is located
+        /// </summary>
+        public AppHost()
+            : base("RedisGeo", typeof(MyServices).Assembly) { }
+
+        /// <summary>
+        /// Application specific configuration
+        /// This method should initialize any IoC resources utilized by your web service classes.
+        /// </summary>
+        public override void Configure(Container container)
+        {
+            container.Register<IRedisClientsManager>(new RedisManagerPool("localhost:6379"));
+
+            //Config examples
+            //this.Plugins.Add(new PostmanFeature());
+            //this.Plugins.Add(new CorsFeature());
+        }
+    }
+}
---
+++ RedisGeo/RedisGeo/MyServices.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ServiceStack;
+using ServiceStack.Redis;
+
+namespace RedisGeo.ServiceInterface
+{
+    [Route("/getgeo")]
+    public class GetGeo : IReturn<GeoResponse>
+    {
+        public double Lng { get; set; }
+
+        public double Lat { get; set; }
+
+        public double Radius { get; set; }
+    }
+
+    public class GeoResponse
+    {
+        public double Longitude { get; set; }
+
+        public double Latitude { get; set; }
+    }
+
+    [Route("/addgeo")]
+    public class AddGeo : IReturn<string>
+    {
+        public double Lng { get; set; }
+
+        public double Lat { get; set; }
+    }
+
+
+    public class MyServices : Service
+    {
+        const string driverKey = "driver";
+        public string user = "user";
+
+        public object Any(AddGeo request)
+        {
+            using (var client = TryResolve<IRedisClientsManager>().GetClient())
+            {
+                using (var pipeline = client.CreateTransaction())
+                {
+                    pipeline.QueueCommand(r => r.AddGeoMember(driverKey, request.Lng, request.Lat, user));
+                    var commited = pipeline.Commit();
+                    return commited ? 1 : 0;
+                }
+            }
+        }
+
+        public object Any(GetGeo request)
+        {
+            using (var client = TryResolve<IRedisClientsManager>().GetClient())
+            {
+                var nativeClient = (RedisNativeClient)client;
+                var drivers = nativeClient.GeoRadius(driverKey,
+                    request.Lng, request.Lat, request.Radius, RedisGeoUnit.Kilometers, withCoords: true, asc: true
+                    );
+
+                Console.WriteLine(drivers[0].Latitude);
+                Console.WriteLine(drivers[0].Longitude);
+
+                return new GeoResponse
+                {
+                    Latitude = drivers[0].Latitude,
+                    Longitude = drivers[0].Longitude
+                };
+            }
+        }
+    }
+}
